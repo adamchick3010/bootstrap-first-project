60d674470a1522cc99ad92db874822a7e437ad07
Bartosz Adamczyk <adamczyk.bartosz@outlook.com>
2021-02-13 01:29:36 +0100

    added all files that make up the first page I created using Bootstrap

---
+++ js/code.js
@@ -0,0 +1,22 @@
+// var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+//     target: '#navbar'
+//   })
+
+//   $(document).click(function (event) {
+//     var clickover = $(event.target);
+//     var _opened = $(".navbar-collapse").hasClass("show");
+//     if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+//         $(".navbar-toggler").click();
+//     }
+// });
+
+
+
+//   //   $(document).click(function (event){
+//       var clickover = $(event.target);
+//       var_opened = $(".navbar-collapse").hasClass("show");
+//       if (_opened === true && !clickover.hasClass("navbar-toggler")){
+//           $(".navbar-toggler").click();
+      
+//       }
+//   });
